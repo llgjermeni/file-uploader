0e0db874b3237a9281444dfb457d8963ce881f38
llgjermeni <laertgjermeni@hotmail.com>
2019-11-10 20:59:47 +0100

    Add the code in github

---
+++ UploadFileApp/Controllers/HomeController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadFileApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        static CloudBlobClient blobClient;
+        const string blobContainerName = "uploadfile-container";
+        static CloudBlobContainer blobContainer;
+
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"].ToString());
+
+                
+                blobClient = storageAccount.CreateCloudBlobClient();
+                blobContainer = blobClient.GetContainerReference(blobContainerName);
+                await blobContainer.CreateIfNotExistsAsync();
+
+                
+                await blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+               
+                List<Uri> allBlobs = new List<Uri>();
+                foreach (IListBlobItem blob in blobContainer.ListBlobs())
+                {
+                    if (blob.GetType() == typeof(CloudBlockBlob))
+                        allBlobs.Add(blob.Uri);
+                }
+
+                return View(allBlobs);
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UploadAsync()
+        {
+            try
+            {
+                HttpFileCollectionBase files = Request.Files;
+                int fileCount = files.Count;
+
+                if (fileCount > 0)
+                {
+                    for (int i = 0; i < fileCount; i++)
+                    {
+                        CloudBlockBlob blob = blobContainer.GetBlockBlobReference(GetRandomBlobName(files[i].FileName));
+                        await blob.UploadFromFileAsync(files[i].FileName);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteImage(string name)
+        {
+            try
+            {
+                Uri uri = new Uri(name);
+                string filename = Path.GetFileName(uri.LocalPath);
+
+                var blob = blobContainer.GetBlockBlobReference(filename);
+                await blob.DeleteIfExistsAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteAll()
+        {
+            try
+            {
+                foreach (var blob in blobContainer.ListBlobs())
+                {
+                    if (blob.GetType() == typeof(CloudBlockBlob))
+                    {
+                        await ((CloudBlockBlob)blob).DeleteIfExistsAsync();
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        private string GetRandomBlobName(string filename)
+        {
+            string ext = Path.GetExtension(filename);
+            return string.Format("{0:10}_{1}{2}", DateTime.Now.Ticks, Guid.NewGuid(), ext);
+        }
+    }
+}
---
+++ UploadFileApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UploadFileApp.MvcApplication" Language="C#" %>
